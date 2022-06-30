2df684e6245664a37ec671dabed2fb047489e027
SulemP <sulee.ths@gmail.com>
2022-06-30 12:32:28 +0200

    Primer commit.

--- vista/asset/js/registros.js
+++ vista/asset/js/registros.js
@@ -1,1 +1,7 @@
 let registros = []
+
+let idID = document.getElementById("idID");
+let idNombre = document.getElementById("idNombre");
+let idApellido = document.getElementById("idApellido");
+let idEdad = document.getElementById("idEdad");
+let tabla = document.getElementById("tabla");
